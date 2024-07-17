955d2eccd7f482606048a20c9b6d77397854d4e8
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-16 20:15:00 -0500

    nueva clase

---
+++ LaMatrix.py
@@ -0,0 +1,10 @@
+# Escribe un programa que solicite al usuario
+# que ingrese su nombre. El nombre de almacenar 
+# en una variable llamada nombre.
+# A continuación se debe mostrar en pantalla 
+# el txto "ahora estas en lamatrix, [usuario]", donde
+# "[usuario]" se reemplazará por el nombre que 
+# el usuario  haya ingresado.
+
+nombre = input("escribe tu nombre")
+print("Ahora estás en la matrix, " + nombre)
