b5a8c53ce2144421bcf7410b366cedc18a2ac76c
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:37:52 -0500

    IVA

---
+++ ValordeIva.py
@@ -0,0 +1,11 @@
+Primernumero=int(input("Ingresar el primer valor: "))
+Segundonumero=int(input("Ingresar el segundo valor: "))
+Tercerumero=int(input("Ingresar el tercer valor: "))
+
+Suma=Primernumero+Segundonumero+Tercerumero
+print("resultado de la suma es: ",Suma)
+
+Promedio=Suma/3
+print("resultado del promedio:",Promedio)
+
+
