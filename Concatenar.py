bc0e8f9a8f4eea9ff02959de022d30a4b1c4608d
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-23 21:10:12 -0500

    clase12

---
+++ Concatenar.py
@@ -0,0 +1,3 @@
+P1=input("palabra 1: ")
+P2=input("palabra 2: ")
+print(P1," ",P2 )
---
+++ Mensajeenpantalla.py
@@ -0,0 +1,9 @@
+edad=int(input("ingresar la edad: "))
+articulos=int(input("ingresar el articulo:"))
+if edad>18:
+    if articulos>1:
+        print(True)
+    else:
+        (False) 
+else:
+    print(False)
